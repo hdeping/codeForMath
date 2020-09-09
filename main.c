a16787a8c75d78fcd6c69150daca8cf3e94ccab8
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:49:35 +0800

    add 36_clique/01_v1

--- main.c
+++ main.c
@@ -1,47 +1,68 @@
 #include "head.h"
-#define n 100
-#define m 10
+#define n 7
+
+/*void print_mat{{{*/
+void print_mat(int (*mat)[n], int size)
+{
+    for(int i = 0;i < size;i++)
+    {
+        for(int j = 0;j < n;j++)
+        {
+            printf("%d ",mat[i][j] );
+            
+
+        }
+        printf("\n");
+        
+    }
+    
+}
+/*}}}*/
 
 /*int main{{{*/
 int main( int argc,char *argv[])
 {
-    srand(time(NULL)); 
-    int arr[n];
 
-    for(int i = 0;i < n;i++)
+    int mat[6][3] = {0,1,4,0,6,5,0,3,2,1,
+                     5,2,2,6,4,1,3,6};
+    int adj[n][n] = {0} ;
+    for(int i = 0;i < 6;i++)
     {
-        arr[i] = i;
+        int tmp1 = mat[i][0] ;
+        int tmp2 = mat[i][1] ;
+        int tmp3 = mat[i][2] ;
+        adj[tmp1][tmp2] = 1;
+        adj[tmp1][tmp3] = 1;
+        adj[tmp2][tmp3] = 1;
+        adj[tmp2][tmp1] = 1;
+        adj[tmp3][tmp1] = 1;
+        adj[tmp3][tmp2] = 1;
     }
+    print_mat(adj,n);
 
-    int tmp;
-    for(int i = 0;i < n;i++)
+
+    int count = 0;
+    for(int i = 0;i < n - 2;i++)
     {
-        tmp  = rand() % n;
-        int swap;
-        swap = arr[tmp];
-        arr[tmp] = arr[i];
-        arr[i]   = swap;
-    }
-    
-    for(int i = 0;i < n;i++)
-    {
-        printf("%d ",arr[i]);
-    }
-    int status[n];
-    
-    int limit = n / m;
-    for(int i = 0;i < m;i++)
-    {
-        for(int j = 0;j < limit;j++)
+        for(int j = i + 1;j < n - 1;j++)
         {
-            int num = i*m + j;
-            status[arr[num]] = i;
+            for(int k = j + 1;k < n;k++)
+            {
+
+                if (  adj[i][j]  == 1 && adj[i][k]  == 1 && adj[j][k]  == 1 )
+                {
+                    count++;
+                }
+                
+            }
+
         }
     }
-    for(int i = 0;i < n;i++)
-    {
-        printf("%d ",status[i]);
-    }
+    printf("count = %d\n",count);
+    
+    
+    
+
 }
 /*}}}*/
 
