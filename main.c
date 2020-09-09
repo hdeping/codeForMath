5723cec0f1c40a6bd2364f10a0e9b2fe748f236a
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:49:04 +0800

    add 29_ConwaySequence/02_v2

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include "head.h"
-#define n (int)2E6
+#define n (int)1E6
+#define cy_times 40
+#define display_stati(arr,len) for(int i = 0;i < len;i++) \
+{ \
+    printf("%d ",arr[i]); \
+} \
+printf("\n");
+
+#define display_stati_double(arr,len) for(int i = 0;i < len;i++) \
+{ \
+    printf("%lf ",arr[i]); \
+} \
+printf("\n");
+
+#define getsum(sum,arr,len) sum = 0;\
+for(int i = 0;i < len;i++) \
+{ \
+    sum += arr[i]; \
+} 
+
 
 /*void lookAndSay{{{*/
 void lookAndSay(char *next,char *previous)
@@ -23,25 +42,57 @@ void lookAndSay(char *next,char *previous)
     
 }
 /*}}}*/
+/*void statistics{{{*/
+void statistics(int *stati,char *conway)
+{
+    int len = strlen(conway);
+    for(int i = 0;i < len;i++)
+    {
+        switch(conway[i])
+        {
+            case '1':
+                stati[0]++;
+                break;
+            case '2':
+                stati[1]++;
+                break;
+            case '3':
+                stati[2]++;
+                break;
+        }
+    }
+}
+/*}}}*/
 /*int main{{{*/
 int main( int argc,char *argv[])
 {
+
     char conway[n];
     char tmp[n];
     // file output
     FILE *fp;
-    fp= fopen("conway.txt","w");
+    fp= fopen("data.txt","r");
     assert(fp != NULL);
-
-    sprintf(conway,"1");
-    fprintf(fp,"%s\n",conway);
-    for(int i = 0;i < 20;i++)
+    
+    // read file and get statistics data
+    int stati[3] = {0};
+    for(int i = 0;i < 51;i++)
     {
-        lookAndSay(tmp,conway);
-        memcpy(conway,tmp,strlen(tmp));
-        // fprintf(fp,"%s\n",conway);
-        printf("%s\n",conway);
+        fgets(conway,n,fp);
+        statistics(stati,conway);
     }
+    
+
+    int sum;
+    getsum(sum,stati,3);
+    double result[3];
+    for(int i = 0;i < 3;i++)
+    {
+        result[i] = (double)stati[i] / sum;
+    }
+    display_stati(stati,3);
+    display_stati_double(result,3);
+    
     fclose(fp);
     
 }
