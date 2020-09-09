463114c3a2d5faa95b86bd8af6e12f6f6f4b41d1
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:49:37 +0800

    add 36_clique/02_v2

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "head.h"
-#define n 7
+#define n 19 
 
 /*void print_mat{{{*/
 void print_mat(int (*mat)[n], int size)
@@ -18,27 +18,74 @@ void print_mat(int (*mat)[n], int size)
     
 }
 /*}}}*/
-
 /*int main{{{*/
 int main( int argc,char *argv[])
 {
 
-    int mat[6][3] = {0,1,4,0,6,5,0,3,2,1,
-                     5,2,2,6,4,1,3,6};
+    int num1[5],num2[3];
     int adj[n][n] = {0} ;
-    for(int i = 0;i < 6;i++)
+    // get data from the file
+    FILE *fp;
+    fp= fopen("data.txt","r");
+    assert(fp != NULL);
+    // part 1: 6*5
+    int m1,m2;
+    m1 = 6;
+    m2 = 5;
+    for(int i = 0;i < m1;i++)
     {
-        int tmp1 = mat[i][0] ;
-        int tmp2 = mat[i][1] ;
-        int tmp3 = mat[i][2] ;
-        adj[tmp1][tmp2] = 1;
-        adj[tmp1][tmp3] = 1;
-        adj[tmp2][tmp3] = 1;
-        adj[tmp2][tmp1] = 1;
-        adj[tmp3][tmp1] = 1;
-        adj[tmp3][tmp2] = 1;
+        for(int j = 0;j < m2;j++)
+        {
+            fscanf(fp,"%d",num1+j);
+            printf("%3d ",num1[j]);
+            
+        }
+        printf("\n");
+        
+
+        for(int j = 0;j < m2 - 1;j++)
+        {
+            for(int k = j + 1;k < m2;k++)
+            {
+                int tmp1 = num1[j];
+                int tmp2 = num1[k];
+                adj[tmp1][tmp2] = 1;
+                adj[tmp2][tmp1] = 1;
+            }
+            
+        }
     }
-    print_mat(adj,n);
+    // part 2: 9*3
+    
+    m1 = 9;
+    m2 = 3;
+    
+    for(int i = 0;i < m1;i++)
+    {
+        for(int j = 0;j < m2;j++)
+        {
+            fscanf(fp,"%d",num2+j);
+            printf("%3d ",num2[j]);
+        }
+        printf("\n");
+        for(int j = 0;j < m2 - 1;j++)
+        {
+            for(int k = j + 1;k < m2;k++)
+            {
+                int tmp1 = num2[j];
+                int tmp2 = num2[k];
+                adj[tmp1][tmp2] = 1;
+                adj[tmp2][tmp1] = 1;
+            }
+            
+            
+        }
+    }
+
+    
+
+    fclose(fp);
+    // print_mat(adj,n);
 
 
     int count = 0;
@@ -52,6 +99,9 @@ int main( int argc,char *argv[])
                 if (  adj[i][j]  == 1 && adj[i][k]  == 1 && adj[j][k]  == 1 )
                 {
                     count++;
+                    printf("%3d %3d %3d\n",i,j,k);
+                    
+
                 }
                 
             }
@@ -65,4 +115,3 @@ int main( int argc,char *argv[])
 
 }
 /*}}}*/
-
