334c2758fb0404da02bc36fb2de609c04547f6a4
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:46:28 +0800

    add 08_physics/02_Jarzynski

--- main.c
+++ main.c
@@ -1,19 +1,52 @@
 #include "head.h"
 
+double kb = 1.38E-23;
+double T  = 298;
+double eV = 1.6E-19;
+double unit;
+
+/*void print_array{{{*/
+void print_array(int *a)
+{
+    for(int i = 0;i < n;i++)
+    {
+        printf("%d,%d\n",i,a[i]);
+        
+    }
+}
+/*}}}*/
+/*double get_delta_free_energy{{{*/
+double get_delta_free_energy(int *a)
+{
+    double res;
+    res = 0.0;
+    for(int i = 0;i < n;i++)
+    {
+        res += exp(- 1.0*a[i]) ;
+    }
+    res /= (1.0*n);
+    res = - log(res);
+    return res;
+}
+/*}}}*/
 /*int main{{{*/
 int main( int argc,char *argv[]){
-    double m,a,u0,hbar;
-    double xi;
-    m    = 9.1E-31;
-    a    = 1.0E-9;
-    u0   = 1.6E-19;
-    hbar = 6.626E-34;
-    xi = sqrt(m*pow(a,2)*u0/(2*pow(hbar,2)));
-    printf("m    = %g\n",m);
-    printf("a    = %g\n",a);
-    printf("u0   = %g\n",u0);
-    printf("hbar = %g\n",hbar);
-    printf("xi   = %lf\n",xi);
+    int *a;
+    a = (int *)malloc(sizeof(int)*n);
+    unit = kb*T/eV;
+    printf("unit    = %g\n",unit);
+    srand(time(NULL)); 
+    for(int i = 0;i < n;i++)
+    {
+        a[i] = rand() % 20 + 1;
+    }
+    //print_array(a);
+    double deltaFree;
+    deltaFree = get_delta_free_energy(a);
+    printf("Delta Free Energy is %lf\n",deltaFree);
+    
+    free(a);
+    
 }
 /*}}}*/
 
