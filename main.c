0b5abf342e5baa9b623fd902fb29c468714aed8e
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:47:30 +0800

    add 19_igraph/04_blas

--- main.c
+++ main.c
@@ -1,107 +1,29 @@
 #include "head.h"
 
-/*int judge_graph{{{*/
-int judge_graph(igraph_t *res,igraph_t *g1,igraph_t *g2,
-                  igraph_vector_t *map1,igraph_vector_t *map2)
-{
-    igraph_compose(res,g1,g2,map1,map2);
-    if ( igraph_ecount(res) != 0  )
-    {
-        printf("igraph_ecount  != 0 \n");
-        igraph_destroy(res);
-        return 0;
-    }
-    if ( igraph_vector_size(map1) && igraph_vector_size(map2))
-    {
-        printf("size of map1 and map2  != 0 \n");
-        igraph_destroy(res);
-        return 1;
-    }
-    return -1;
-}
-/*}}}*/
 /*int main{{{*/
 int main( int argc,char *argv[]){
-    igraph_t g1,g2,res;
-    igraph_vector_t v;
-    igraph_vector_t map1,map2;
+    igraph_matrix_t m;
+    igraph_vector_t x,y;
 
-    igraph_vector_init(&map1,1);
-    igraph_vector_init(&map2,1);
-    // composition with the empty graph
+    igraph_vector_init_real(&x,3,1.0,2.0,3.0);
+    igraph_vector_init_real(&y,4,4.0,5.0,6.0,7.0);
 
-    printf("UnDirected\n");
-    igraph_empty(&g1,5,IGRAPH_UNDIRECTED);
-    igraph_full(&g2,5,IGRAPH_UNDIRECTED,IGRAPH_NO_LOOPS);
-    int num;
-    num = judge_graph(&res,&g1,&g2,&map1,&map2);
-    if ( num == 1 )
+    igraph_matrix_init(&m,4,3);
+    for(int i = 0;i < 4;i++)
     {
-        return 11;
+        for(int j = 0;j < 3;j++)
+        {
+            MATRIX(m,i,j) = i + j + 1;
+        }
     }
-    else if(num == 0 )
-    {
-        return 1;
-    }
-    printf("new res\n");
-    num = judge_graph(&res,&g2,&g1,&map1,&map2);
-    if ( num == 1 )
-    {
-        return 12;
-    }
-    else if(num == 0 )
-    {
-        return 2;
-    }
-    igraph_destroy(&g1);
-    igraph_destroy(&g2);
-    printf("Directed\n");
-    igraph_empty(&g1,5,IGRAPH_DIRECTED);
-    igraph_full(&g2,5,IGRAPH_DIRECTED,IGRAPH_NO_LOOPS);
-    num = judge_graph(&res,&g1,&g2,&map1,&map2);
-    if ( num == 1 )
-    {
-        return 11;
-    }
-    else if(num == 0 )
-    {
-        return 1;
-    }
-    printf("new res\n");
-    num = judge_graph(&res,&g2,&g1,&map1,&map2);
-    if ( num == 1 )
-    {
-        return 12;
-    }
-    else if(num == 0 )
-    {
-        return 2;
-    }
-    igraph_destroy(&g1);
-    igraph_destroy(&g2);
 
-    igraph_vector_init_int_end(&v,-1,0,1,1,2,5,6,-1);
-    igraph_create(&g1,&v,0,IGRAPH_UNDIRECTED);
-    igraph_vector_destroy(&v);
+    igraph_blas_dgemv(0,2,&m,&x,3,&y);
+    igraph_vector_print(&y);
+    igraph_vector_print(&x);
+    igraph_matrix_print(&m);
 
-    igraph_vector_init_int_end(&v,-1,0,1,0,4,5,6,-1);
-    igraph_create(&g2,&v,0,IGRAPH_UNDIRECTED);
-    igraph_vector_destroy(&v);
-
-    igraph_compose(&res,&g1,&g2,&map1,&map2);
-    FILE *fp;
-    fp= fopen("edgelist.txt","a+");
-    assert(fp != NULL);
-
-    igraph_write_graph_edgelist(&res,fp);
-    fclose(fp);
-
-    igraph_destroy(&g1);
-    igraph_destroy(&g2);
-    igraph_destroy(&res);
-    igraph_vector_print(&map1);
-    igraph_vector_print(&map2);
-    igraph_vector_destroy(&map1);
-    igraph_vector_destroy(&map2);
+    igraph_vector_destroy(&x);
+    igraph_vector_destroy(&y);
+    igraph_matrix_destroy(&m);
 }
 /*}}}*/
