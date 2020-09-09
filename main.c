b6ffe8d90e3f4f7b1e6252576e76058b8055817a
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:47:49 +0800

    add 19_igraph/13_v13

--- main.c
+++ main.c
@@ -1,56 +1,57 @@
 #include "head.h"
-#define n 1000
-#define m 20
-#define cycles 1000
+#define n 100
+#define m 10
+#define cycles 100
 
-/*void getDegreeDist{{{*/
-void getDegreeDist(float *deg_seq)
+/*int main{{{*/
+int main( int argc,char *argv[])
 {
+    srand(time(NULL)); 
     igraph_t graph;
     igraph_t g1;
     igraph_full(&g1,10,IGRAPH_UNDIRECTED ,IGRAPH_NO_LOOPS );
     igraph_vector_t res;
     igraph_vector_init(&res,0);
-    for(int i = 0;i < cycles;i++)
-    {
-        igraph_barabasi_game(&graph,n,1.0,m,0,1,1,IGRAPH_UNDIRECTED ,
-                IGRAPH_BARABASI_BAG,&g1);
-        igraph_degree(&graph,&res,igraph_vss_all(),
+    FILE *fp;
+    fp= fopen("karate.gml","r");
+    assert(fp != NULL);
+    igraph_read_graph_gml(&graph,fp);
+    fclose(fp);
+    fp= fopen("karate.dot","w");
+    igraph_write_graph_dot(&graph,fp);
+    assert(fp != NULL);
+    fclose(fp);
+
+
+    // igraph_barabasi_game(&graph,n,1.0,m,0,1,1,IGRAPH_UNDIRECTED ,
+       // IGRAPH_BARABASI_BAG,&g1);
+    igraph_degree(&graph,&res,igraph_vss_all(),
                 IGRAPH_ALL,IGRAPH_NO_LOOPS );
-        int degree;
-        for(int i = 0;i < n;i++)
-        {
-            degree = VECTOR(res)[i];
-            deg_seq[degree]++;
-        }
+    // get assortativity
+    igraph_real_t ass = 1.0;
+
+    // res denotes the degree of each node
+    ass = igraph_assortativity_nominal(&graph,&res,&ass,IGRAPH_UNDIRECTED );
+
+    for(int i = 0;i < n;i++)
+    {
+        printf("%f\n",(float)VECTOR(res)[i]);
+        
     }
+    
+    
+    printf("assortativity is %f\n",(float)ass);
+    
+    ass = igraph_assortativity(&graph,&res,NULL,&ass,IGRAPH_UNDIRECTED );
+    
+    printf("NO nominal!\n");
+    
+    printf("assortativity is %f\n",(float)ass);
+
     igraph_vector_destroy(&res);
     igraph_destroy(&g1);
     igraph_destroy(&graph);
-}
-/*}}}*/
-/*int main{{{*/
-int main( int argc,char *argv[])
-{
-
-    float deg_seq[n] = {0.0};
-    getDegreeDist(deg_seq);
-    FILE *fp;
-    char filename[30];
-    sprintf(filename,"deg_seq_%d_%d.txt",n,m);
-    fp= fopen(filename,"w");
-    assert(fp != NULL);
-    for(int i = 0;i < n;i++)
-    {
-        if ( deg_seq[i] )
-        {
-            fprintf(fp,"%d %f\n",i,deg_seq[i]/(n*cycles));
-        }
-    }
-    char command[40];
-    sprintf(command,"ln -f -s %s data.txt",filename);
-    system(command);
-    fclose(fp);
+    
     return 0;
 }
 /*}}}*/
