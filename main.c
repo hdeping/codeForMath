726bc8c785f69d5760d84b471f149618ca14fb62
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:49:08 +0800

    add 29_ConwaySequence/04_analysis

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include "head.h"
 #define n (int)1E7
+#define cy_times 40
+#define display_stati(arr,len) for(int i = 0;i < len;i++) \
+{ \
+    printf("%d ",arr[i]); \
+} \
+printf("\n");
+
+#define display_stati_double(arr,len) for(int i = 0;i < len;i++) \
+{ \
+    printf("%lf ",arr[i]); \
+} \
+printf("\n");
+
+#define getsum(sum,arr,len) sum = 0;\
+for(int i = 0;i < len;i++) \
+{ \
+    sum += arr[i]; \
+} 
+
+
 
 char conway[n];
-char tmp[n];
 /*void lookAndSay{{{*/
 void lookAndSay(char *next,char *previous)
 {
@@ -25,40 +44,97 @@ void lookAndSay(char *next,char *previous)
     
 }
 /*}}}*/
+/*void statistics{{{*/
+void statistics(int *stati,char *conway)
+{
+    int len = strlen(conway);
+    for(int i = 0;i < len;i++)
+    {
+        switch(conway[i])
+        {
+            case '1':
+                stati[0]++;
+                break;
+            case '2':
+                stati[1]++;
+                break;
+            case '3':
+                stati[2]++;
+                break;
+        }
+    }
+}
+/*}}}*/
+/*void statistics2{{{*/
+void statistics2(int *stati,char *conway)
+{
+    int len = strlen(conway);
+    int num;
+    for(int i = 0;i < len - 2;i++)
+    {
+        num = (conway[i] - '1')*3 + conway[i+1] - '1';
+        stati[num]++;
+    }
+}
+/*}}}*/
+/*void statistics3{{{*/
+void statistics3(int *stati,char *conway)
+{
+    int len = strlen(conway);
+    int num;
+    for(int i = 0;i < len - 3;i++)
+    {
+        num = (conway[i] - '1')*9 + (conway[i+1] - '1')*3
+            + conway[i+2] - '1';
+        stati[num]++;
+    }
+}
+/*}}}*/
+/*void print_result{{{*/
+void print_result(int *stati,int len)
+{
+    
+    int sum;
+    getsum(sum,stati,len);
+    double *result;
+    result = (double *)malloc(sizeof(double)*len);
+    for(int i = 0;i < len;i++)
+    {
+        result[i] = (double)stati[i] / sum;
+    }
+    display_stati(stati,len);
+    display_stati_double(result,len);
+    free(result);
+
+}
+/*}}}*/
 /*int main{{{*/
 int main( int argc,char *argv[])
 {
-    if ( argc == 1  )
-    {
-        printf("Please input a file\n");
-        printf("For Example: '<command> <data.file>'\n");
-        return 0;
-    }
-    
+
     // file output
     FILE *fp;
-    fp= fopen("conway.txt","w");
+    fp= fopen("data.txt","r");
     assert(fp != NULL);
-    clock_t start,end;
-
-
-    sprintf(conway,"1");
-    fprintf(fp,"%s\n",conway);
-    start = clock();
-    for(int i = 0;i < atoi(argv[1]);i++)
+    
+    // read file and get statistics data
+    int stati[3] = {0};
+    int stati2[9] = {0};
+    int stati3[27] = {0};
+    while ( !feof(fp) )
     {
-        lookAndSay(tmp,conway);
-        memcpy(conway,tmp,strlen(tmp));
-        fprintf(fp,"%s\n",conway);
-        
+        fgets(conway,n,fp);
+        statistics(stati,conway);
+        statistics2(stati2,conway);
+        statistics3(stati3,conway);
     }
-    end = clock();
-    fclose(fp);
-    fp= fopen("time.txt","a+");
-    assert(fp != NULL);
-    fprintf(fp,"index : %s,time is %lf\n",argv[1],(double)(end - start) / CLOCKS_PER_SEC);
-    printf("time is %lf\n",(double)(end - start) / CLOCKS_PER_SEC);
-    fclose(fp);
+    
+    print_result(stati,3);
+    print_result(stati2,9);
+    print_result(stati3,27);
 
+    
+    fclose(fp);
+    
 }
 /*}}}*/
