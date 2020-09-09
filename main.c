01d9523b29f575f8bb802150d7d63f5b86fa1e96
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:48:49 +0800

    add 26_lapackAndBlas/01_blas/03_v3

--- main.c
+++ main.c
@@ -1,23 +1,4 @@
-/* -*- Mode:C; Coding:us-ascii-unix; fill-column:132 -*- */
-/* ****************************************************************************************************************************** */
-/**
-   @file      blas2C.c
-   @author    Mitch Richling <https://www.mitchr.me/>
-   @Copyright Copyright 1997 by Mitch Richling.  All rights reserved.
-   @brief     Demonstrate several cblas (level 1) functions. @EOL
-   @Keywords  blas cblas C fortran numerical linear algebra vector matrix gemv ger
-   @Std       C89
-
-   This is a simple program intended to illistrate how to make use of #gemv and #ger blas routines (as implimented in the cblas).
-              
-*/
-
-/* ------------------------------------------------------------------------------------------------------------------------------ */
-
-#include <math.h>               /* Math stuff      ISOC  */
-#include <stdio.h>              /* I/O lib         ISOC  */
-#include <stdlib.h>             /* Standard Lib    ISOC  */
-#include <cblas.h> 
+#include "head.h"
 
 /*void printVector{{{*/
 void printVector(double *num,int len)
@@ -43,27 +24,43 @@ void printMatrix(double *num,int row,int column)
 /*}}}*/
 /*int main{{{*/
 int main(int argc, char **argv) {
-  double a[4*5] = {  1, 2, 3, 4, 5,
+  double a[4*5] = {  1, 2, 3, 4, 5,  /* CblasRowMajor */
                      6, 7, 8, 9,10,
                     11,12,13,14,15,
                     16,17,18,19,20
                   };
-  double x[5]  = {2,3,4,5,6};
-  double y[4];
+  double b[5*4] = {  1, 0, 0, 0,   /* CblasRowMajor */
+                     0, 0, 1, 0,
+                     0, 1, 0, 0,
+                     0, 0, 0, 1,
+                     0, 0, 0, 0
+                  };
+  double c[4*4];
 
-  printVector(x,5);
-  printMatrix(a,4,5);
+  double d[4*4] = {  1, 2, 3, 4,  /* CblasRowMajor */
+                     6, 7, 8, 9,
+                    11,12,13,14,
+                    16,17,18,19
+                  };
+  double e[4*4] = {  1, 0, 0, 0,  /* CblasRowMajor */
+                     0, 0, 1, 0,
+                     0, 1, 0, 0,
+                     0, 0, 0, 1
+                  };
 
-           /* row_order      transform     lenY lenX alpha  a  lda  X  incX  beta  Y, incY */
-  CBLAS_LAYOUT layout  = CblasRowMajor;
-  CBLAS_TRANSPOSE tran = CblasNoTrans;
-  cblas_dgemv(layout, tran, 4,   5,   1,     a,   5, x, 1,    0,    y, 1);
-  printVector(y,4);
+   printMatrix(a, 4, 5);
+   printMatrix(b, 5, 4);
+            /* row_order      transform     transform     rowsA colsB K  alpha  a  lda  b  ldb beta c   ldc */
+   cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 4,    4,    5, 1.0,   a,   5, b, 4,  0.0, c,  4);
+   printMatrix(c, 4, 4);
 
-          /* row_order       lenY lenX alpha  X  incX  Y, incY A  LDA */
-  cblas_dger(layout,  4,   5,   1,     y, 1,    x, 1,   a, 5);
-  printMatrix(a,4,5);
 
-  return 0;
+   printMatrix(d, 4, 4);
+   printMatrix(e, 4, 4);
+            /* row_order      transform     transform     rowsA colsB K  alpha  a  lda  b  ldb beta c   ldc */
+   cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, 4,    4,    4, 1.0,   d,   4, e, 4,  0.0, c,  4);
+   printMatrix(c, 4, 4);
+
+   return 0;
 } /* end func main */
 /*}}}*/
