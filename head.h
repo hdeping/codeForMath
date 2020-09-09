9ed58d7287d5c9be5f71f3964b466355d769c6dc
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:47:20 +0800

    add 18_Dijkstra/02_cVersion

--- head.h
+++ head.h
@@ -2,5 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
-#define n 8
+#include <stdbool.h>
+
+#define n 5
+#define limitStack 20
+#define MAX (int)1E8
+#define limitStack 20
+
+struct Stack
+{
+    int arr[limitStack];
+    int length;
+    int top;
+};
+typedef struct Stack stack;
+
+struct Graph
+{
+    int matrix[n][n];      //邻接矩阵 
+    int vertex;                 //顶点数 
+    int edge;                 //边数 
+}; 
+typedef struct Graph graph;
 
