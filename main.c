3a4f83ae6b1a76f71f7866b1a38d6e8b56bb3784
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:50:05 +0800

    add 40_ResidueTheorem/04_compute

--- main.c
+++ main.c
@@ -1,199 +1,40 @@
 #include "head.h"
 
-int recordNum[100];
 
-/*void printData{{{*/
-void printData(int *data,int n,int m)
+/*int getRes{{{*/
+int getRes(int a1,int b1,int a2,int b2)
 {
-    for(int i = 0;i < n;i++)
+    int res;
+    res = 0 ;
+
+    int count = 1;
+    while ( (count*a2)%a1 != 1  )
     {
-        for(int j = 0;j < m;j++)
-        {
-            int index = i*m + j;
-            printf("%3d ",data[index]);
-            
-        }
-        printf("\n");
-        
+        count++;
     }
-}
-/*}}}*/
-/*void getMatMul{{{*/
-void getMatMul(int *out,int *arr1,int *arr2,int size)
-{
-    int index1,index2,index3;
-    for(int i = 0;i < size;i++)
+    res += b1*a2*count; 
+    while ( (count*a1)%a2 != 1  )
     {
-        for(int j = 0;j < size;j++)
-        {
-            index1 = i*size + j;
-            int sum = 0;
-
-            for(int k = 0;k < size;k++)
-            {
-                index2 = i*size + k;
-                index3 = k*size + j;
-                sum += arr1[index2]*arr2[index3];
-
-            }
-            out[index1] = sum;
-            
-        }
-        
+        count++;
     }
-    
-}
-/*}}}*/
-/*int  getResidueMat{{{*/
-int getResidueMat(int *arr,int value,int finalNum,int size)
-{
-    int *arr1,*result;
-    int *arr2;
+    res += b2*a1*count; 
+    return (res%(a1*a2));
 
-    result = (int *)malloc(sizeof(int)*4);
-    arr1 = (int *)malloc(sizeof(int)*4);
-    arr2 = (int *)malloc(sizeof(int)*4);
-    
-    int *tmp;
-
-    result[0] = 1;
-    result[1] = 0;
-    result[2] = 0;
-    result[3] = 1;
-    
-    for(int i = 0;i < size;i++)
-    {
-        arr1[0] = 1;
-        arr1[3] = 1;
-        int j = i%2;
-        arr1[j+1] = 0;
-        arr1[2-j] = -arr[i];
-        // arr1[2-j] = arr[size - 1 - i];
-        // printData(arr1,2,2);
-        // exchange arr2 and result
-        tmp  = arr2;
-        arr2 = result;
-        result = tmp;
-
-        getMatMul(result,arr2,arr1,2);
-
-        
-    }
-    printf("Result is :\n");
-    printData(result,2,2);
-
-
-    // get final solutions!
-    if ( size%2 == 0  )
-    {
-        arr1[0] = 0;
-        arr1[1] = 1;
-        arr1[2] = value;
-        arr1[3] = - finalNum;
-    }
-    else
-    {
-        arr1[0] = value;
-        arr1[1] = - finalNum;
-        arr1[2] = 0;
-        arr1[3] = 1;
-        
-    }
-    
-    
-    getMatMul(arr2,result,arr1,2);
-    printf("a: (%d) + (%d)t \n",arr2[0],arr2[1]);
-    printf("a: (%d) + (%d)t \n",arr2[2],arr2[3]);
-    int num = arr2[0] % arr2[1];
-    if ( num < 0 )
-    {
-        num += arr2[1]; 
-    }
-    
-    printf("a: %d\n",num);
-
-
-
-    
-    free(arr1);
-    free(arr2);
-    free(result);
-    return num;
-}
-/*}}}*/
-/*void runResidue{{{*/
-void runResidue(int num1,int num2,int value)
-{
-    int tmp;
-    int i = 0;
-    while ( num2 != 1  )
-    {
-        recordNum[i] = num1 / num2;
-        tmp = num1%num2;
-        num1 = num2;
-        num2 = tmp;
-        i++;
-        
-    }
-    int n = i;
-    printf("%d,%d n = %d\n",num1,num2,n);
-    printData(recordNum,1,n);
-
-
-    // num1 is the final number
-    // num1,num2 = finalNum,1
-    getResidueMat(recordNum,value,num1,n);
-
-}
-/*}}}*/
-/*int getResNum{{{*/
-int getResNum(int *arrN,int *arrM,int size)
-{
-    int *arrData,*res;
-
-    arrData = (int *)malloc(sizeof(int)*size);
-    res = (int *)malloc(sizeof(int)*size);
-    int totalMul = 1;
-    for(int i = 0;i < size;i++)
-    {
-        totalMul *= arrN[i];  
-    }
-    for(int i = 0;i < size;i++)
-    {
-        arrData[i] = totalMul/arrN[i];
-    }
-    for(int i = 0;i < size;i++)
-    {
-        res[i] = runResidue(arrData[i],arrN[i],size);
-    }
-    
-
-    
-    free(arrData);
-    free(res);
-    
 }
 /*}}}*/
 /*int main{{{*/
 int main( int argc,char *argv[])
 {
-    // n*a + m*c = value
-    // what is the integer solution?
-    // bigger one is in the front
-    // such as : 73 27!
+    int resNum[4] ;
 
-    // There is no need that n > m
-    // n > m or m > n, anyway!
-
-    int arrN[3] = {3,5,7}; 
-    int arrM[3] = {1,2,3}; 
+    resNum[0] = getRes(5,4,6,3);
+    resNum[1] = getRes(7,5,8,1);
+    resNum[2] = getRes(15,0,28,12);
+    // resNum[2] = getRes(5*6,resNum[0],7*8,resNum[1]);
+    // resNum[3] = getRes(5*6*7*8,resNum[2],9,0);
+    printf("num is %d , %d \n",resNum[0],resNum[1]);
+    printf("num is %d , %d \n",resNum[2],resNum[3]);
     
-
-    // runResidue(n,m,value);
-    int num = getResNum(arrN,arrM,3);
-    printf("num is %d\n",num);
-    
-
 }
 /*}}}*/
 
