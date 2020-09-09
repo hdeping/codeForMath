c177ec4ba08b8f40bcf62ae9f96fb321aca7380c
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:49:58 +0800

    add 40_ResidueTheorem/01_v1

--- head.h
+++ head.h
@@ -3,6 +3,3 @@
 #include <time.h>
 #include <assert.h>
 
-#define n 10
-#define total (1.0*RAND_MAX)
-
--- main.c
+++ main.c
@@ -1,85 +1,116 @@
 #include "head.h"
 
-/*int cmp_int{{{*/
-int cmp_int(const void *a, const void *b)
+/*void printData{{{*/
+void printData(int *data,int n,int m)
 {
-    return *(int *)a - *(int *)b;
-}
-/*}}}*/
-/*int cmp_char{{{*/
-int cmp_char(const void *a, const void *b)
-{
-    return *(char *)a - *(char *)b;
-}
-/*}}}*/
-/*int cmp_double{{{*/
-int cmp_double(const void *a, const void *b)
-{
-    return *(double *)a>*(double *)b?1:-1;
-}
-/*}}}*/
-/*void testInt{{{*/
-void testInt()
-{
-    int arr[n];
     for(int i = 0;i < n;i++)
     {
-        arr[i] = rand() % 100;
-        printf("%d\n",arr[i]);
+        for(int j = 0;j < m;j++)
+        {
+            int index = i*m + j;
+            printf("%3d ",data[index]);
+            
+        }
+        printf("\n");
         
     }
-    qsort(arr,n,sizeof(arr[0]),cmp_int);
-    
-    printf("\n");
-    
-    for(int i = 0;i < n;i++)
-    {
-        printf("%d\n",arr[i]);
-    }
-    
 }
 /*}}}*/
-/*void testChar{{{*/
-void testChar()
+/*void getMatMul{{{*/
+void getMatMul(int *out,int *arr1,int *arr2,int size)
 {
+    int index1,index2,index3;
+    for(int i = 0;i < size;i++)
+    {
+        for(int j = 0;j < size;j++)
+        {
+            index1 = i*size + j;
+            int sum = 0;
 
-    char ch[40] = ",kieurdkfdkfjeiteijfdjfdgdababekkdfdktfe";
-    printf("%s\n",ch);
-    qsort(ch,40,sizeof (char),cmp_char);
-    printf("%s\n",ch);
-    
-}
-/*}}}*/
-/*void testDouble{{{*/
-void testDouble()
-{
-    
-    double arrNew[n];
-    
-    for(int i = 0;i < n;i++)
-    {
-        arrNew[i] = (rand() / total) * 100.0;
-        printf("%lf\n",arrNew[i]);
+            for(int k = 0;k < size;k++)
+            {
+                index2 = i*size + k;
+                index3 = k*size + j;
+                sum += arr1[index2]*arr2[index3];
+
+            }
+            out[index1] = sum;
+            
+        }
         
     }
-    qsort(arrNew,n,sizeof (arrNew[0]),cmp_double);
-    printf("\n");
     
-    for(int i = 0;i < n;i++)
-    {
-        printf("%lf\n",arrNew[i]);
-    }
+}
+/*}}}*/
+/*void getResidueMat{{{*/
+void getResidueMat(int *arr,int value,int size)
+{
+    int *arr1,*result;
+    int *arr2;
 
+    result = (int *)malloc(sizeof(int)*4);
+    arr1 = (int *)malloc(sizeof(int)*4);
+    arr2 = (int *)malloc(sizeof(int)*4);
+    
+    int *tmp;
+
+    result[0] = 1;
+    result[1] = 0;
+    result[2] = 0;
+    result[3] = 1;
+    
+    for(int i = 0;i < size;i++)
+    {
+        arr1[0] = 1;
+        arr1[3] = 1;
+        int j = i%2;
+        arr1[j+1] = 0;
+        arr1[2-j] = -arr[i];
+        // arr1[2-j] = arr[size - 1 - i];
+        // printData(arr1,2,2);
+        // exchange arr2 and result
+        tmp  = arr2;
+        arr2 = result;
+        result = tmp;
+
+        getMatMul(result,arr2,arr1,2);
+
+        
+    }
+    printf("Result is :\n");
+    printData(result,2,2);
+
+
+    // get final solutions!
+    arr1[0] = value;
+    arr1[1] = - 2;
+    arr1[2] = 0;
+    arr1[3] = 1;
+    getMatMul(arr2,result,arr1,2);
+    printf("a: (%d) + (%d)t \n",arr2[0],arr2[1]);
+    printf("a: (%d) + (%d)t \n",arr2[2],arr2[3]);
+    
+
+
+
+    
+    free(arr1);
+    free(arr2);
+    free(result);
 }
 /*}}}*/
 /*int main{{{*/
 int main( int argc,char *argv[])
 {
-    srand(time(NULL)); 
+    int num[5] = {2,1,2,2,1};
+    int n   = 5;
 
-    testInt();
-    testChar();
-    testDouble();
-
+    // 73*a + 27*c = value
+    // what is the integer solution?
+    // bigger one is in the front
+    // such as : 73 27!
+    int value = 1;
+    getResidueMat(num,value,5);
 }
 /*}}}*/
+
