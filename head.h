fec5480dd4b548ee6b2a3ed1b93b2229bfc26746
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:50:17 +0800

    add 44_235Problem/02_v2

--- head.h
+++ head.h
@@ -2,4 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#include <math.h>
 
+struct sequence
+{
+    int ii;
+    int jj;
+    int kk;
+};
+typedef struct sequence sequence;
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-CC = gcc -g
+CC = gcc -g -lm
 option = -o exe
 object = main.o
 source = main.c
