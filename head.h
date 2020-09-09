c837f4d2b47204283e020bb744ced926a8ab847d
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 22:46:44 +0800

    add 14_Hanoi/01_v1

--- head.h
+++ head.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <assert.h>
+
--- main.c
+++ main.c
@@ -1,46 +1,35 @@
 #include "head.h"
 
+char *disk[10] = {"disk0", "disk1", "disk2", "disk3", "disk4",
+"disk5", "disk6", "disk7", "disk8", "disk9"};
+int num = 0;
+/*void hanoi{{{*/
+// move from ch1 ot ch2 using ch3
+void hanoi(int n,char ch1,char ch2,char ch3)
+{
+    assert(n > 0);
+    if ( n == 1  )
+    {
+        printf("ch1 ch2 %c %c\n",ch1,ch2);
+        sleep(1);
+        printf("move %s from %c to %c\n",disk[n-1],ch1,ch2);
+        num++;
+    }
+    else
+    {
+        hanoi(n-1,ch1,ch3,ch2);
+        printf("ch1 ch2 %c %c\n",ch1,ch2);
+        sleep(1);
+        printf("move %s from %c to %c\n",disk[n-1],ch1,ch2);
+        num++;
+        hanoi(n-1,ch3,ch2,ch1);
+    }
+    
+}
+/*}}}*/
 /*int main{{{*/
 int main( int argc,char *argv[]){
-    if ( argc == 1  )
-    {
-        printf("Please input a file\n");
-        printf("For Example: '<command> <data.file>'\n");
-        return 0;
-    }
-    FILE *fp;
-    fp= fopen(argv[argc-1],"r");
-    const int n = 50;
-    const double threshold = 1.90;
-    int *col1,*col2,*col3;
-    double *res;
-    col1 = (int *)malloc(sizeof(int)*n);
-    col2 = (int *)malloc(sizeof(int)*n);
-    col3 = (int *)malloc(sizeof(int)*n);
-    res  = (double *)malloc(sizeof(double)*n);
-    for(int i = 0;i < n;i++)
-    {
-        fscanf(fp,"%d%d%d",col1+i,col2+i,col3+i);
-        res[i] = (double)col2[i]/(double)col3[i];
-        if ( res[i] > threshold )
-        {
-            printf("%d\n",i+1);
-            
-        }
-    }
-    fclose(fp);
-    fp= fopen("new.txt","w");
-    for(int i = 0;i < n;i++)
-    {
-        fprintf(fp,"%5d%5d%5d%10.5lf\n",col1[i],col2[i],
-                col3[i],res[i]);
-    }
-    fclose(fp);
-
-    free(col1);
-    free(col2);
-    free(col3);
-    free(res);
-    return 1;
+    hanoi(3,'A','B','C');
+    printf("moving times are %d\n",num);
 }
 /*}}}*/
